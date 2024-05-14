e3b3fb4bfc2a8f928860e965682ecb4a29e13834
Marcelo-Gallo <marceloaugustogallo@gmail.com>
2024-05-13 23:48:13 -0300

    Registro de chamados

---
+++ registra_chamado.php
@@ -0,0 +1,20 @@
+<?php
+
+    //trabalhando a montagem do texto
+    $titulo = str_replace('#', '-', $_POST['titulo']);
+    $categoria = str_replace('#', '-', $_POST['categoria']);
+    $descricao = str_replace('#', '-', $_POST['descricao']);
+    //implode(#, $_POST); -> poderia ser usado como alternativa
+
+    $texto = $titulo . '#' . $categoria . '#' . $descricao . PHP_EOL;
+    //PHP_EOL ->php end of line
+
+    //abrindo arquivo
+    $arquivo = fopen('arquivo.txt', 'a');
+    //escrevendo arquivo
+    fwrite($arquivo, $texto);
+    //fechando arquivo
+    fclose($arquivo);
+
+    header('Location: abrir_chamado.php');
+?>
